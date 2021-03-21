cfd764822ecf73608c835afc3afb3db53ddf402d
mbiombani <mbiombanistefen@gmail.com>
2021-03-20 22:13:58 -0400

    Fin de la version 2

--- API/Startup.cs
+++ API/Startup.cs
@@ -30,7 +30,7 @@ namespace API
 
             });
            services.AddControllers();
-
+            services.AddCors();
 
         }
         
@@ -45,8 +45,12 @@ namespace API
             }
 
             app.UseHttpsRedirection();
+
             app.UseRouting();
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+
             app.UseAuthorization();
+
             app.UseAuthentication();
 
 
